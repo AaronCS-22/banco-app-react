74e0f352b53ef9784105b9be37ebc7a700ff4bf4
AaronCS-22 <centenoa22@gmail.com>
2025-02-24 10:47:56 +0100

    Componentes de la aplicación estructurados

---
+++ src/Welcome.jsx
@@ -0,0 +1,25 @@
+function Welcome(){
+    return <>
+    <nav>
+        <p className="welcome">Log in to get started</p>
+        <img src="logo.png" alt="Logo" className="logo" />
+        <form className="login">
+          <input
+            type="text"
+            placeholder="user"
+            className="login__input login__input--user"
+          />
+          {/* In practice, use type="password" */}
+          <input
+            type="text"
+            placeholder="PIN"
+            maxLength="4"
+            className="login__input login__input--pin"
+          />
+          <button className="login__btn">&rarr;</button>
+        </form>
+      </nav>
+      </>
+}
+
+export default Welcome;
